c4346a30f720142f941e692ff5fb681dd8f8a51f
rkvisle <rkvisle@shaw.ca>
2014-06-02 16:40:55 -0600

    Plus One Level
    Plus One Level Filter

---
+++ Procurement/ViewModel/Filters/ForumExport/PlusOneLevelAllGems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters
+{
+    class PlusOneLevelAllGems : StatFilter
+    {
+        public PlusOneLevelAllGems() 
+            : base("Plus Gem Level", "Plus to Level of gems in item", "to Level of Gems in this item")
+        { }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+    }
+}
+
